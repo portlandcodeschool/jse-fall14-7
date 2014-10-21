23bd96395656ed729c59b698d09ee41e3a0c6467
Dallasss7 <mr.dalsummers@gmail.com>
2014-10-20 18:23:04 -0700

    what I have so far

--- template/memory-game.js
+++ template/memory-game.js
@@ -2,8 +2,125 @@ var MemoryGame = (function() {
 
 	function Memory(GuiCtor,cardset,gameoverFn) {
 
+		var faceupValuesArr = [];
+
+		var board = cardset.slice();
+
+		this.reset = function(){
+
+			//create new game board			
+			board = cardset.slice();
+			
+			//shuffling the entire array
+			board.sort(shuffle);
+
+			faceupValuesArr = []
+
+			return 'ready to play!';
+
+		};
+
+		
+		this.faceupWhere = function(){
+
+			if(faceupValuesArr[0] !== undefined){
+
+				return board.indexOf('face up');
+			}
+			// return false;
+		};
+		
+		
+		this.faceupValue = function(){
+
+			if(faceupValuesArr.length > 0){
+			
+				return faceupValuesArr[0];
+			}
+			
+			return false;
+		};
+
+		this.remaining = function(){
+			return Object.keys(board).map(Number)
+		};
+
+		
+		this.lift = function(where){
+	
+			var lifted = board[where];
+	
+			if(lifted !== undefined){
+				
+				if(faceupValuesArr[0] === undefined){
+
+					faceupValuesArr.push(lifted);
+
+					board.splice(where,1, 'face up');
+
+					if(displayFn){
+
+						return displayFn(faceupValuesArr[0]);
+					}
+					else{
+
+						return this.faceupValue();
+					}
+				}
+
+				else if(matchFn(lifted, this.faceupValue())){
+
+					console.log(lifted + ', ' + this.faceupValue());
+
+					board.splice(where,1);
+
+					faceupValuesArr.shift();
+
+					board.splice(board.indexOf('face up'),1);
+
+					return 'Match!';
+					}
+				else{
+
+					displayFn(lifted);
+
+					faceupValuesArr.shift();
+
+					return 'no match! Try again';
+				}
+			}
+							
+			if(board.length === 0){
+				
+				if(endgameFn){
+
+					return endgameFn();
+				}
+				else{
+					return "No more pairs! You Won!";
+				}
+			}
+		
+		};
+
 
 	}
 
 	return Memory;
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- template/memory-gui.js
+++ template/memory-gui.js
@@ -1,25 +1,124 @@
-var MemoryGUI = (function () {
-
-	//...
+var MemoryGUI = (function (){
 
 	function GUI(len,clickFn,resetGameFn) {
 
-		// public methods:
-		this.reset = function() {
-			//...
-		}
-		this.show = function(where,what) {
-			//...
-		}
-		this.removeSoon = function(whereArr) {
-			//...
-		}
-		this.hideSoon = function(whereArr) {
-			//...
+		if(Math.sqrt(len) % 1 !== 0){
+
+			var boardW = Math.floor(Math.sqrt(len));
+		
+			var boardH = Math.ceil(len/boardW);
+			
 		}
 
-		//...
-	}
+		var boardW = Math.sqrt(len);
+
+		var boardH = Math.sqrt(len);
+
+		// var container = document.getElementById('memorygame')
+
+		var table = document.createElement('table');
+	
+		table.id = "gametable";
+
+
+		var totalTd = 0;
+		
+		for (var row=0; row<boardH; ++row) {
+			
+			var tr = document.createElement('tr');
+			
+			table.appendChild(tr);
+
+			for (var col=0; col<boardW; ++col) {
+
+				if(totalTd < len){
+				
+					var td = document.createElement('td');
+						
+					tr.appendChild(td);
+					
+					td.id = totalTd++;
+
+					td.classList.add('facedown');
+
+					clickFn(td);
+				}
+			}
+		}
+
+		var memorygame = document.getElementById('memorygame');
+
+		memorygame.appendChild(table);	
+
+		var resetBttn = document.createElement('button');
+
+		memorygame.insertBefore(resetBttn,table);
+
+		var rsttxt = document.createTextNode('reset');
+
+		resetBttn.appendChild(rsttxt);
+
+		resetBttn.classList.add('resetBttn');
+
+		resetBttn.addEventListener('click', function(evt){ td.classList.add('facedown'); });
+
+		
+		
+
+		// public methods:
+		this.reset = function() {
+
+			td.classList.add('reset');
+			
+
+		};
+
+		this.show = function(where,value) {
+
+			where = td.id;
+
+			where.classList.add('faceup');
+
+		};
+
+		this.removeSoon = function(whereArr) {
+			//...
+		};
+		this.hideSoon = function(whereArr) {
+			//...
+		};
+
+		var rst = this.reset;
+	}	
 
 	return GUI;
 })();
+
+function clickFn1(td){
+
+	td.addEventListener('click',function(evt){
+
+		td.classList.add('faceup');
+
+		console.log(td.id);
+	});
+
+
+
+
+}
+
+
+
+function go(){
+	MemoryGUI(52,clickFn1);
+}
+
+window.addEventListener('load',go);
+
+
+
+
+
+
+

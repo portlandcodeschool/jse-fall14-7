a615922ce4c56f1e9d2af17422b9650c42e69ccb
nguidry <nguidry7@gmail.com>
2014-10-20 17:45:17 -0700

    updating to push for deadline

--- nguidry/template/memory-game.js
+++ nguidry/template/memory-game.js
@@ -1,8 +1,101 @@
 var MemoryGame = (function() {
 
-	function Memory(GuiCtor,cardset,gameoverFn) {
 
+	function Memory(GuiCtor,cardset,matchFn,gameoverFn) {
+		//need to account for input of GuiCtor, no interface if null, else build currentGUI
+		var currentGUI = new MemoryGUI(length,lift,reset);
 
+		var slots, //sparse array: will have elements deleted as cards are removed
+			length,//total slots, including gaps
+			there; //position of face-up card if any, or false
+
+		var reset = function() {  //public method
+			slots = cardset.slice();
+			length = cardset.length;
+			there = false;
+			shuffle(slots);
+		}
+		reset();// reset now as part of init'ing
+
+		var faceupValue = function() {
+			return valueAt(there);
+		}
+		var faceupWhere = function() {
+			return there;
+		}
+		var remainsAt = function(where) {
+			return slots[where]!==undefined;
+		}
+		var valueAt = function(where) {
+			return slots[where];
+		}
+		var removeAt = function(where) {
+			delete slots[where];
+		}
+		var remaining = function() {
+			return Object.keys(slots).map(Number);
+		}
+		var checkGameover = function() {
+			if (gameOverFn && (remaining().length === 0))
+				gameOverFn();
+		}
+
+		var lift = function(here) {
+			if (!isValid(here,length)) return false;
+			if (!remainsAt(here)) return false;
+			if (there===here) return false;
+
+			// must be a face-down card here; proceed...
+			var valHere = valueAt(here);
+			//show card in gui
+			currentGUI.show(here,valHere);
+			if (there === false) {
+				// no current face-up
+				there = here; //turn here face-up
+			} else {
+				// check match with face-up
+				if (matchFn(valHere,valueAt(there))) {
+					// match; remove both:
+					removeAt(here);
+					removeAt(there);
+					checkGameover();
+				}
+				//either way, turn face-up to face-down:
+				there = false;
+			}
+
+			return valHere;
+		}
+
+		// Make some methods public:
+		this.reset = reset;
+		this.lift = lift;
+		this.faceupValue = faceupValue;
+		this.faceupWhere = faceupWhere;
+		this.remaining = remaining;
+	}//end ctor
+
+	// Private Functions shared by all boards:
+	//  (these could just as easily be placed inside ctor)
+	function isValid(where,length) {
+			return (typeof where === 'number')
+				&& (where%1 === 0)
+				&& (where>=0)
+				&& (where<length)
+		}
+
+	function shuffle(array) {
+	// Knuth-Fisher-Yates, modified from http://bost.ocks.org/mike/shuffle/
+		var end = array.length, temp, i;
+				// While there remain elements to shuffle…
+		while (end>1) {
+				// Pick a remaining element…
+				i = Math.floor(Math.random() * end--);
+				// And swap it with the current element.
+				temp = array[end];
+				array[end] = array[i];
+		    array[i] = temp;
+			}
 	}
 
 	return Memory;
--- nguidry/template/memory-gui.js
+++ nguidry/template/memory-gui.js
@@ -1,73 +1,75 @@
 var MemoryGUI = (function () {
 
-
-	function prepareForClicks(elem,clickFn,row,col) {
-		if (!elem) return;
-		elem.addEventListener("click",function(){clickFn(row,col);});
-	}
-
-	function rowColToId(row,col) {
-		return 'row'+row+'col'+col;
-	}
-
-	function makeTable(len) {
-		var table = document.createElement('table');
-		table.setAttribute('id','gameBoard');
-
-		var tr, 
-			td,
-			side = Math.ceil(Math.sqrt(len));
-
-		for (var row = 0; row<side; row++) {
-			tr = document.createElement('tr');
-			table.appendChild(tr);
-			for (var col = 0; col<side; col++) {
-				td = document.createElement('td');
-				td.id = rowColToId(col,row);
-
-				prepareForClicks(td,col,row);
-				tr.appendChild(td);
-			}
-		}
-		return table;
-	}
-
 	function GUI(len,clickFn,resetGameFn) {
-		var table = makeTable(len);
-		document.getElementById("memorygame").appendChild(table);
 
-		this.board = table; //???
+		var memorygame = document.getElementById("memorygame");
 
-		// this.drawChecker = function(row,col,drawFn) {
-		// 	var sqr = document.getElementById(rowColToId(row,col));
-		// 	return (sqr && drawFn(sqr));
+		function prepareForClicks(elem,clickFn) {
+			if (!elem) return;
+			elem.addEventListener("click", clickFn);
+		}
+
+		function makeTable(len) {
+			var table = document.createElement("table");
+			table.setAttribute("id","gameBoard");
+
+			var tr, 
+				side = Math.ceil(Math.sqrt(len));
+
+			for (var row = 0; row<side; row++) {
+				tr = document.createElement("tr");
+				table.appendChild(tr);
+
+				for (var col = 0; col<side; col++) {
+
+					td = document.createElement("td");
+					td.id = "row"+row+"col"+col;
+					td.position = (row*side) + col;
+					td.classList.add("face-down");
+
+					prepareForClicks(td,clickFn);
+					tr.appendChild(td);
+				}
+			}
+			return table;
+		}
 
 		// public methods:
 		this.reset = function() {
-			//...
-		}
+			var allTds = document.getElementsByTagName("td");
+			allTds.classList.remove("matched");
+			addTds.classList.remove("face-up");
+			addTds.classList.add("face-down");
+		};
+
 		this.show = function(where,what) {
-			//probably not td...
-			//td[where].classList.add('face-up');
-			//**The display value of that card (according to 
-			//the game model) will be provided as parameter value.
-		}
+			//change the class from face-down to face-up
+			td[where].classList.remove("face-down");
+			td[where].classList.add("face-up");
+		};
+
 		this.removeSoon = function(whereArr) {
-			//whereArr.classList.add('face-down');
-		}
+			//whereArr.classList.add("face-down");
+		};
 
+		setTimeout(this.removeSoon, 500);
+		
 		this.hideSoon = function(whereArr) {
-			//whereArr.classList.add('matched');
-		}
+			//whereArr.classList.add("matched");
+		};
 
-		//setTimeout(this.removeSoon, 500);
-		//setTimeout(this.hideSoon, 500);
+		setTimeout(this.hideSoon, 500);
+
+
+		var table = makeTable(len);
+		memorygame.appendChild(table);
+
+		var resetButton = document.createElement("button");
+		resetButton.id = "resetButton";
+		memorygame.appendChild(button);
 	}
-
-	this.reset = reset;
-	this.show = show;
-	this.removeSoon = removeSoon;
-	this.hideSoon = hideSoon;
 
 	return GUI;
 })();
+
+console.log("lkadjfalskdf");
